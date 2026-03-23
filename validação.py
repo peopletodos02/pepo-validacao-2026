92a2b86a3357824e6ead08b351f3659a7983ee36
peopletodos02 <peopletodos02@gmail.com>
2026-03-23 16:08:37 -0300

    validação.py

--- validação.py
+++ validação.py
@@ -67,7 +67,7 @@ if df_base is not None:
 
     if gestor_sel:
         # --- CONFIGURAÇÃO DA EQUIPE (MAIOR E EM NEGRITO) ---
-        st.markdown("<br><h3 style='font-weight: bold;'>Configuração da Equipe:</h3>", unsafe_allow_html=True)
+        st.markdown("<br><h3 style='font-weight: bold;'></h3>", unsafe_allow_html=True)
         tipo_avaliacao = st.radio(
             "**A equipe será avaliada por pares do mesmo setor?**",
             ["Sim", "Não"], index=0, horizontal=True, key="tipo_av"
